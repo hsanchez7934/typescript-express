daca7239ca912311e9ef27110c0e03d35b4bfd67
Hector Sanchez <hsanchez7934@gmail.com>
2020-03-28 00:07:10 -0600

    Initialize app

--- build/index.js
+++ build/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('test');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+app.get('/', function (request, response) {
+    response.send("\n\t\t<div>\n\t\t\t<h1>Hi There!</h1>\n\t\t</div>\n\t");
+});
+app.listen(3000, function () { return console.log('Listening on port 3000.'); });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('test')
+import express, {Request, Response} from 'express';
+
+const app = express();
+
+app.get('/', (request: Request, response: Response) => {
+	response.send(`
+		<div>
+			<h1>Hi There!</h1>
+		</div>
+	`);
+});
+
+app.listen(3000, () => console.log('Listening on port 3000.'));
